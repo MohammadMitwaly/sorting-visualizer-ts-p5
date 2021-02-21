8981d1e87f8a57a13646c0b3325a6d0142138893
Mo. Mitwaly <mhmadmtwale@gmail.com>
2021-02-21 13:28:49 +0200

    react-p5 issue

---
+++ src/Components/MainSketch.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Sketch from "react-p5"
+import p5Types from "p5"; //Import this for typechecking and intellisense
+
+interface MainSketchProps {
+	//Your component props
+}
+
+export const MainSketch: React.FC<MainSketchProps> = (props: MainSketchProps) => {
+	let x = 50;
+	const y = 50;
+
+	//See annotations in JS for more information
+	const setup = (p5: p5Types, canvasParentRef : Element) => {
+		p5.createCanvas(500, 500).parent(canvasParentRef);
+	};
+
+	const draw = (p5: p5Types) => {
+		p5.background(0);
+		p5.ellipse(x, y, 70, 70);
+		x++;
+	};
+
+	return <Sketch setup={setup} draw={draw} />;
+};
+
